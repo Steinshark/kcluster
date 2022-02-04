d9e6539afd4ef436ce2508ffe66bf4aa41970c47
Steinshark <everettperson@gmail.com>
2022-02-04 11:16:16 -0500

    indent fix

--- naive.py
+++ naive.py
@@ -31,44 +31,44 @@ def create_csr_matrix(filename,header=3,verbose=False):
         n_words         = int(file.readline())
         n_words_total   = int(file.readline())
 
-            # Create a dictionary so we can map word_ID to actual text in the NYTIMES doc
-            vocab = {}
-            with open(vocabNYT,'r') as vocab_file:
-                for i, word in enumerate(vocab_file.readlines()):
-                    vocab[i] = word
+        # Create a dictionary so we can map word_ID to actual text in the NYTIMES doc
+        vocab = {}
+        with open(vocabNYT,'r') as vocab_file:
+            for i, word in enumerate(vocab_file.readlines()):
+                vocab[i] = word
 
-                    # define the size of the dataset we will build
-                    rows = n_articles	+	1
-                    cols = n_words		+	1
-                    printc(f"attempting construction of matr: ({rows},{cols})","BLUE")
-                    # initialize an lil matrix (faster to fill)
-                    matrix = lil_matrix((rows,cols), dtype = np.float64)
-
-
-                    # Step through each article and see which word appeared in it
-                    docwords = {}
-                    for line in file:
-                        doc, word, count = line.split(' ')
-                        #input(f"doc: {doc}, word: {word}, count: {count}")
-                        matrix[int(doc),int(word)] = int(count)
-                        try:
-                            docwords[int(doc)].append(count)
-                        except KeyError:
-                            docwords[int(doc)] = [int(count)]
-
-                            return matrix.todense(), docwords
+                # define the size of the dataset we will build
+                rows = n_articles	+	1
+                cols = n_words		+	1
+                printc(f"attempting construction of matr: ({rows},{cols})","BLUE")
+                # initialize an lil matrix (faster to fill)
+                matrix = lil_matrix((rows,cols), dtype = np.float64)
 
 
-                            def tf_calc(csr_matr):
-                                strs = []
-                                print(csr_matr.shape[0])
-                                for i in range(csr_matr.shape[0]):
-                                    strs.append(' '.join(map(str,[i for i,x in enumerate(csr_matr[i].toarray()[0]) if not x == 0])))
-                                    print(strs[:20])
+                # Step through each article and see which word appeared in it
+                docwords = {}
+                for line in file:
+                    doc, word, count = line.split(' ')
+                    #input(f"doc: {doc}, word: {word}, count: {count}")
+                    matrix[int(doc),int(word)] = int(count)
+                    try:
+                        docwords[int(doc)].append(count)
+                    except KeyError:
+                        docwords[int(doc)] = [int(count)]
+
+                        return matrix.todense(), docwords
 
 
-                                    def SVD_decomp(csr_matr,k=100):
-                                        return svds(sparse_matrix,k=150)
+                        def tf_calc(csr_matr):
+                            strs = []
+                            print(csr_matr.shape[0])
+                            for i in range(csr_matr.shape[0]):
+                                strs.append(' '.join(map(str,[i for i,x in enumerate(csr_matr[i].toarray()[0]) if not x == 0])))
+                                print(strs[:20])
+
+
+                                def SVD_decomp(csr_matr,k=100):
+                                    return svds(sparse_matrix,k=150)
 
 
 if __name__ == "__main__":
