9b08e03ec57a45b2b6d5be7bc797aa9e22de8431
Steinshark <everettperson@gmail.com>
2022-02-07 01:27:53 -0500

    final commit

--- ex.sh
+++ ex.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=we<3ML:)
-#SBATCH --output=out.txt
+#SBATCH --output=writeout
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=20
 #SBATCH --time=1:00:00
 #SBATCH --partition=hpc
 
 cd /home/m226252/kcluster
-python3 execute.py full 3500 none f t 1 2 1 
+python3 execute.py full 3500 preSVD.npz f t 1 2 1
